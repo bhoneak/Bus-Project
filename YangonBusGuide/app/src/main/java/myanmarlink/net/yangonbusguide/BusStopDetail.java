5ccd95df8f9479db841bfbd4db5e5674a148d60d
bhone aung kyaw <bhoneak@gmail.com>
2017-01-08 16:20:09 +0630

    update

---
+++ YangonBusGuide/app/src/main/java/myanmarlink/net/yangonbusguide/BusStopDetail.java
@@ -0,0 +1,13 @@
+package myanmarlink.net.yangonbusguide;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BusStopDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bus_stop_detail);
+    }
+}
